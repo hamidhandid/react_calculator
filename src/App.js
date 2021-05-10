360d13f5a970dd08e344412381e15291d7097e18
Hamid <montazeria90@gmail.com>
2021-05-10 16:00:02 +0430

    Add initial implementation of calculator functions

--- src/App.js
+++ src/App.js
@@ -8,26 +8,98 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      screenText: "123",
+      screenText: "0",
     };
   }
   handlePressDigit = (digit) => {
-    // todo
+    this.setState((prev) => {
+      const screenText = prev.screenText
+      return {
+        screenText: digit === "0" && screenText === "0" ? screenText : screenText !== "0" ? screenText + digit.toString() : digit
+      }
+    })
   };
   handlePressOperator = (operator) => {
-    // todo
+    let screenText = this.state.screenText.toString();
+    const containsOperator = this.hasOperator(screenText)
+    if (containsOperator) {
+      this.setState((prev) => {
+        try {
+          return {
+            screenText: eval(prev.screenText).toString() + operator
+          }
+        } catch {
+          const last = screenText.substring(screenText.length - 1)
+          if (this.hasOperator(last) || last === "-") {
+            return {
+              screenText: prev.screenText.toString().substring(0, screenText.length - 1) + operator
+            }
+          }
+          return {
+            screenText: "error"
+          }
+        }
+      })
+    } else {
+      this.setState((prev) => {
+        return {
+          screenText: prev.screenText + operator
+        }
+      })
+    }
   };
+  hasOperator = (str) => {
+    str = str.toString()
+    return str.includes("+") || (str.length > 1 && str.substring(1).includes("-"))
+     || str.includes("/") || str.includes("*") || str.includes("%")
+  }
   handlePressAC = () => {
-    // todo
+    this.setState((_) => {
+      return {
+        screenText: "0"
+      }
+    })
   };
   handlePressDot = () => {
-    // todo
+    this.setState((prev) => {
+      const screenText = prev.screenText;
+      if (screenText.includes(".")) {
+        return;
+      }
+      return {
+        screenText: screenText + "."
+      }
+    })
   };
   handlePressNegator = () => {
-    // todo
+    this.setState((prev) => {
+      const screenText = prev.screenText.toString();
+      if (screenText === "0") {
+        return
+      }
+      if (screenText.startsWith("-")) {
+        return {
+          screenText: screenText.substring(1)
+        }
+      } else {
+        return {
+          screenText: "-" + screenText
+        }
+      }
+    })
   };
   handlePressResult = () => {
-    // todo
+    this.setState((prev) => {
+      try {
+        return {
+          screenText: eval(prev.screenText).toString()
+        }
+      } catch {
+        return {
+          screenText: "error"
+        }
+      }
+    })
   };
   render() {
     return (
